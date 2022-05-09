fee841f040370349da8d53d00d7284f56fc176be
smifaye <smifaye@gmail.com>
2022-05-09 15:30:56 +0100

    IT WORKS!

--- background.js
+++ background.js
@@ -1,29 +1,24 @@
-async function getCurrentTab() {
-  let queryOptions = {
-    active: true,
-    currentWindow: true
-  };
-  let [tab] = await chrome.tabs.query(queryOptions);
-  return tab;
-}
-
-if (tab == ["https://www.citizensadvice.org.uk/family/*", "https://www.citizensadvice.org.uk/immigration/*"]) {
-  chrome.action.onClicked.addListener(function(tab) {
+chrome.action.onClicked.addListener((tab) => {
+  if (tab.url.includes("/family/")) {
     chrome.scripting.executeScript({
       target: {
-        tabID: tab.id
+        tabId: tab.id
       },
-      files: ['bookmarklet.js']
-    })
-  });
-}
-else {
-  chrome.action.onClicked.addListener(function(tab) {
+      files: ["bookmarklet.js"]
+    });
+  } else if (tab.url.includes("/immigration/")) {
     chrome.scripting.executeScript({
       target: {
-        tabID: tab.id
+        tabId: tab.id
       },
-      files: ['epi-bookmarklet.js']
-    })
-  });
-}
+      files: ["bookmarklet.js"]
+    });
+  } else {
+    chrome.scripting.executeScript({
+      target: {
+        tabId: tab.id
+      },
+      files: ["epi-bookmarklet.js"]
+    });
+  }
+});
