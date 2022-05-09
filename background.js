7f5b4650b24c56bdfcd4181a9f7bad66349dd664
smifaye <smifaye@gmail.com>
2022-05-09 11:54:35 +0100

    Test

--- background.js
+++ background.js
@@ -1,8 +1,24 @@
-chrome.action.onClicked.addListener(function(tab) {
-  chrome.scripting.executeScript({
-    target: {
-      tabId: tab.id
-    },
-    files: ["bookmarklet.js"]
-  })
+const contentful = ["https://www.citizensadvice.org.uk/family/*","https://www.citizensadvice.org.uk/immigration/*"];
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status == 'complete' && contentful.some(url => tab.url.includes(url))) {
+    chrome.action.onClicked.addListener(function(tab) {
+      chrome.scripting.executeScript({
+        target: {
+          tabId: tab.id
+        },
+        files: ["bookmarklet.js"]
+      })
+    });
+  }
+  else {
+    chrome.action.onClicked.addListener(function(tab) {
+      chrome.scripting.executeScript({
+        target: {
+          tabId: tab.id
+        },
+        files: ["bookmarklet.js"]
+      })
+    });
+  }
 });
