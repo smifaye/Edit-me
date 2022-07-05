2a22bc41f40b68dc902702757f4991af4f63730e
smifaye <smifaye@gmail.com>
2022-07-05 14:05:25 +0100

    Update guid.js

--- guid.js
+++ guid.js
@@ -3,9 +3,7 @@ function getGUID() {
   for (var i = 0; i < metas.length; i++) {
     if (metas[i].getAttribute("name") == "cab-guid") {
       return metas[i].getAttribute("content");
-    } else {
-      return ("blank - this page is already on Contentful");
-    }
+    } 
   }
   return "";
 }
