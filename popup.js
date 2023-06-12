3c667216c46cfb341d054dce51e8a43c47989033
smifaye <smifaye@gmail.com>
2023-06-12 11:54:29 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -75,7 +75,6 @@ view.addEventListener("click", async () => {
 });
 
 login.addEventListener("click", async () => {
-  let tab = await getCurrentTab();
   
   chrome.tabs.update(
     url: "https://epi.citizensadvice.org.uk",
